25ef6388051bad7451ae11fefcc60f56ec0dfd7d
NikitaOvramenko <ovramenko.nikitka@gmail.com>
2025-12-18 20:45:44 -0800

    changed bg of why us section

--- TCS-Junk-Removal-Frontend/src/home-page/WhyUs.tsx
+++ TCS-Junk-Removal-Frontend/src/home-page/WhyUs.tsx
@@ -71,6 +71,7 @@ export default function WhyUs() {
         src={bgPic}
         alt=""
       />
+      <div className="filter absolute w-full h-full bg-green-900 opacity-65"></div>
       <div className="absolute inset-0 z-[1] bg-black/55 pointer-events-none" />
 
       <div className="content relative z-10 flex flex-col justify-center gap-8 overflow-x-clip px-4 md:px-8 h-full">
