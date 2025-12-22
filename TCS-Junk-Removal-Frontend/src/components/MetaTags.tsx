db3eaf96016120dc422ba6ee31b6688b958df2c8
NikitaOvramenko <ovramenko.nikitka@gmail.com>
2025-12-21 23:19:48 -0800

    added robot.txt to tell google bots what to crawl and also added canonical tag for better SEO

--- TCS-Junk-Removal-Frontend/src/components/MetaTags.tsx
+++ TCS-Junk-Removal-Frontend/src/components/MetaTags.tsx
@@ -1,8 +1,14 @@
 import { useLocation } from "../context/LocationContext";
+import { useLocation as useRouterLocation } from "react-router-dom";
 
 export default function MetaTags() {
   const { location } = useLocation();
-  console.log(location?.meta.title);
+  const { pathname } = useRouterLocation();
+
+  const domain = "https://tcs-junk-removal.vercel.app";
+
+  const cleanPath = pathname === "/" ? "" : pathname.replace(/\/$/, "");
+  const canonicalUrl = `${domain}${cleanPath}`;
 
   return (
     <>
@@ -11,6 +17,7 @@ export default function MetaTags() {
         name="description"
         content={location?.meta.description ?? "Fast junk removal services"}
       />
+      <link rel="canonical" href={canonicalUrl} />
     </>
   );
 }
